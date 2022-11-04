8503f47b7a453c9ecaa11a916bd330f375a55a75
Megha P <megha.p@experionglobal.local>
2022-11-04 09:35:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log('Hello from node app');
+// var passenger = {
+//     name: 'Passenger 1',
+//     email: 'passenge@mail.com',
+//     age: 45,
+//     phone: [12345, 123456]
+// }
+// var data = JSON.stringify(passenger);
+// console.log(data);
+//{"name":"Passenger 1","email":"passenge@mail.com","age":45,"phone":[12345,123456]}     object in string format
+
+// var text = '{"name":"Passenger 1","email":"passenge@mail.com","age":45,"phone":[12345,123456]}'
+// var data = JSON.parse(text);
+// console.log(data);
